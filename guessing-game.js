a3af34a7bc9e6d257747b0281bdac64f30d2c17d
Kendra <cruickshank.kendra@gmail.com>
2024-04-17 16:44:26 -0700

    2nd part

--- guessing-game.js
+++ guessing-game.js
@@ -1,4 +1,51 @@
-const secretNumber = 10;
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let minRange = 0;
+let maxRange = 100;
+
+const secretNumber = randomInRange(minRange, maxRange)
+
+function randomInRange(min, max) {
+
+    return Math.floor(Math.random() * (max - min) + min);
+    // function getRandomIntInclusive(min, max) {
+    //     const minCeiled = Math.ceil(min);
+    //     const maxFloored = Math.floor(max);
+    //     return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+    //   }
+}
+
+const askRange = () => {
+    rl.question("Enter a min number", + (minAnswer) => {
+        const min = Number(minAnswer);
+        rl.question("Enter a max number", + (maxAnswer) => {
+            const max = Number(maxAnswer);
+            console.log("I'm think of a number between " + min + " and " + max);
+        });
+    });
+
+    return (min, max);
+}
+
+
+
+// Const handleFirstQuestion = (answer) => {
+// 	console.log(“hello” + answer);
+// 	rl.question(“Where are you from?”, handleSecondQuestion);
+// };
+
+//  Const handleSecondQuestion = (answer) => {
+// 	console.log(answer + “ is a great place!”);
+// 	rl.question(“How are you doing? ”, handleThirdQuestion);
+// };
+
+//rl.question(“What is your name? “, handleFirstQuestion);
+
+
 
 function checkGuess(number) {
     if(number > secretNumber){
@@ -13,13 +60,6 @@ function checkGuess(number) {
     }
 }
 
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
 function askGuess(){
     rl.question('Enter a guess: ', (answer) => {
         const guess = Number(answer);
@@ -33,4 +73,6 @@ function askGuess(){
         }
     });
 }
-askGuess();
+
+
+//askGuess();
